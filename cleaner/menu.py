af9e91a287e3761d214ae023be3fb30b2eaf96fd
qwerty <itsqwz@gmail.com>
2023-02-04 17:50:26 +0300

    Move os functions wrappers to utils.py

--- cleaner/menu.py
+++ cleaner/menu.py
@@ -6,42 +6,10 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from collections import namedtuple
 
+from . import utils
+
 
 LOG = logging.getLogger('app')
-
-
-def safe_mkdir(p: str):
-    try:
-        os.mkdir(p)
-        return 0
-    except FileExistsError:
-        return 1
-
-
-def safe_mkdirs(p: str):
-    try:
-        os.makedirs(p)
-        return 0
-    except FileExistsError:
-        return 1
-
-
-def rmove_dir(dir_out: str, dir_in: str):  # recursion move
-    """
-    Move the shortcuts separately, not the entire folder as a whole. Otherwise,
-    the built-in service shortcuts with translated names may not be displayed
-    correctly.
-    """
-    for entry in os.scandir(dir_out):
-        entry: os.DirEntry
-
-        if entry.is_dir():
-            new_dir_in = os.path.join(dir_in, entry.name)
-            safe_mkdir(new_dir_in)
-            rmove_dir(entry.path, new_dir_in)
-
-        else:
-            os.replace(entry.path, os.path.join(dir_in, os.path.basename(entry.path)))
 
 
 class StartMenuDir:
@@ -112,8 +80,8 @@ class StartMenuFolder(SMFolder):
 
     def move(self, path_to_directory: str) -> None:
         new_path = os.path.join(path_to_directory, self.name)
-        safe_mkdir(new_path)
-        rmove_dir(self.path, new_path)
+        utils.safe_mkdir(new_path)
+        utils.rmove_dir(self.path, new_path)
 
         self.delete()
         self.path = new_path
@@ -175,7 +143,7 @@ class StartMenuShortcut(SMObject):
     def relative_move(self, path_to_directory: str):
         new_path_to_dir = os.path.join(path_to_directory, os.path.split(self.get_rpath())[0])
         if not os.path.exists(new_path_to_dir):
-            safe_mkdirs(new_path_to_dir)
+            utils.safe_mkdirs(new_path_to_dir)
 
         self.move(new_path_to_dir)
 
---
+++ cleaner/utils.py
@@ -0,0 +1,35 @@
+import os
+
+
+def safe_mkdir(p: str):
+    try:
+        os.mkdir(p)
+        return 0
+    except FileExistsError:
+        return 1
+
+
+def safe_mkdirs(p: str):
+    try:
+        os.makedirs(p)
+        return 0
+    except FileExistsError:
+        return 1
+
+
+def rmove_dir(dir_out: str, dir_in: str):  # recursion move
+    """
+    Move the shortcuts separately, not the entire folder as a whole. Otherwise,
+    the built-in service shortcuts with translated names may not be displayed
+    correctly.
+    """
+    for entry in os.scandir(dir_out):
+        entry: os.DirEntry
+
+        if entry.is_dir():
+            new_dir_in = os.path.join(dir_in, entry.name)
+            safe_mkdir(new_dir_in)
+            rmove_dir(entry.path, new_dir_in)
+
+        else:
+            os.replace(entry.path, os.path.join(dir_in, os.path.basename(entry.path)))
