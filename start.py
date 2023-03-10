5fc70c9bf3187cbd60bbff8769e1ebfba93df3d8
qwerty <itsqwz@gmail.com>
2023-03-10 21:27:31 +0300

    Add "--log-clean" start arg which toggles KEEP_LOG_FILE

--- start.py
+++ start.py
@@ -1,7 +1,21 @@
+import argparse
 import cleaner
 
 
+parse = argparse.ArgumentParser('Start Menu Cleaner')
+parse.add_argument(
+    '--log-clean',
+    action=argparse.BooleanOptionalAction,
+    help='',
+    default=False,
+)
+
+
 def main():
+    args = parse.parse_args()
+    if args.log_clean:
+        cleaner.menu.SMCleaner.LOG.KEEP_LOG_FILE = True
+
     cleaner.LOG.info('Application start')
     app = cleaner.widgets.QApplication([])
     window = cleaner.MainWindow()
